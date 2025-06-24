6406f8c849f50935f9b81ff4e4c9ba6349278d4d
Bpritha2002 <prithabanerjee881@gmail.com>
2025-06-24 21:49:25 +0530

    fixed APi issue

--- src/Helper/Helper.js
+++ src/Helper/Helper.js
@@ -1,16 +1,41 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-let adminUrl = "https://api.coincap.io/v2"
-if(process.env?.REACT_APP_ENV === "production"){
-    adminUrl = "https://api.coincap.io/v2"
+// let adminUrl = "https://api.coincap.io/v2"
+// if(process.env?.REACT_APP_ENV === "production"){
+//     adminUrl = "https://api.coincap.io/v2"
 
+// }
+
+// export const baseURL = adminUrl;
+
+// let axiosInstance = axios.create({
+//     baseURL
+// })
+
+// export default axiosInstance;
+
+import axios from "axios";
+
+const API_KEY =
+  "1317a146ca92a69c6594d0ec72520980ffa863ac8de54ecdbce1b8e021ca54cd";
+
+// Use new CoinCap v3 base URL
+let adminUrl = "https://rest.coincap.io/v3";
+
+if (process.env?.REACT_APP_ENV === "production") {
+  adminUrl = "https://rest.coincap.io/v3";
 }
 
 export const baseURL = adminUrl;
 
-let axiosInstance = axios.create({
-    baseURL
-})
-
+const axiosInstance = axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${API_KEY}`,
+  },
+  params: {
+    apiKey: API_KEY,
+  },
+});
 
 export default axiosInstance;
